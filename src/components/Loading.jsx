07af275094501b4b9c038af93b631651e54a7f6a
thomasravache <thomasravache31@gmail.com>
2021-10-07 12:45:35 -0300

    Aprimorando componente de Loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Col } from 'react-bootstrap';
+import LoadingImage from './LoadingImage';
+import './styles/loading.css';
 
 const Loading = () => (
-  <div className="loading-container"><i className="fas fa-spinner loading" /></div>
+  // <div className="loading-container"><i className="fas fa-spinner loading" /></div>
+  <div className="loading-container">
+    <Col xs={ 12 } md={ 4 }>
+      <LoadingImage />
+    </Col>
+  </div>
 );
 
 export default Loading;
